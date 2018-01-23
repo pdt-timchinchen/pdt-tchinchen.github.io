0f4e56ce3b1d20815f34d779973a0f7fe2ce6837
tim <tchinchen@pagerduty.com>
2018-01-23 09:34:59 +0000

    MandS Demo for BMC

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /* Set rates + misc */
-var taxRate = 0.0;
-//var shippingRate = 15.00;
-var shippingRate = 1.00; 
+var taxRate = 0.05;
+var shippingRate = 5.00;
+//var shippingRate = 1.00; 
 var fadeTime = 300;
 
 
